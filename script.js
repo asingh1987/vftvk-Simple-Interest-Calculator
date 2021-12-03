2c408a5431c22300975a66b5883ac952497b42d9
asingh1987 <80244846+asingh1987@users.noreply.github.com>
2021-12-02 19:38:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
+            {
+                var principal = document.getElementById("principal").value;                
+                var rate = document.getElementById("rate").value;
+                var years = document.getElementById("years").value;
+                var interest = principal * years * rate /100;
+                var year = new Date().getFullYear()+parseInt(years);
+				if (principal>0) {
+				document.getElementById("result").innerHTML="If you deposit <mark>"+ principal+"</mark><br/>"+"at an interest rate of <mark>"+rate+"</mark><br/>"+"You will receive an amount of <mark>"
+				+interest+"</mark><br/>"+"in the year <mark>"+year+"</mark>";
+				}else{				
+				alert('Enter a Positive number');
+				document.getElementById("principal").focus();
+				}
+        }
+        function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
-}
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+} 
         
